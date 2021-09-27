c2882dd455ccee96ab83a79ee7497166439ce579
mertcakmak2 <mertcakmak2@gmail.com>
2021-09-28 00:33:25 +0300

    docker compose and debezium config file

---
+++ src/main/java/com/project/debeziumpostgre/DebeziumPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.project.debeziumpostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebeziumPostgreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebeziumPostgreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/debeziumpostgre/DebeziumPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.debeziumpostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebeziumPostgreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
